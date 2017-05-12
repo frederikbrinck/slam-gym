991a76212f6d4e82a19440e8b5ea07d72dc2713a
Mufei Li <mufeili1996@gmail.com>
2017-05-12 16:50:09 -0400

    Created LandmarkDatabase
    It tracks a whole landmark database as well as a new temporary landmark database.
    - Given observations of the form [x1 y1; x2 y2; x3 y3,...xn yn], it does data association, discard useless landmarks (those that are 
       not distinguishable enough) and keep new landmarks separate.
    - After we perform EKF update, it provides a function that adds new landmarks to the landmark database and reinitialize the 
       temporary landmark database.

---
+++ LandmarkDatabase.m
@@ -0,0 +1,115 @@
+classdef LandmarkDatabase < handle
+    properties
+        landmarks = [];
+        maxAssociateError = 0.1
+        maxLandmarkError = 0.2;
+
+        % New landmarks do not need to get updated through EKF. Keep
+        % them separate and add them to our landmark database obj.landmarks
+        % after updating through EKF.
+        newLandmarks = [];
+    end
+    
+    methods
+        function obj = LandmarkDatabase(associateError, landmarkError)
+           if nargin == 2
+               obj.maxAssociateError = associateError;
+               obj.maxLandmarkError = landmarkError;
+           end
+        end
+
+        function extractLandmarks(obj, observations)
+           % Apply obj.associateLandmark to all observations.
+           for i = 1:length(observations)
+               obj.associateLandmark(observations(i,1),observations(i,2));
+           end
+        end
+        
+        function lm = associateLandmark(obj, x, y)
+           % There are three cases.
+           % 1. If the point is associated to an existing landmark, 
+           %    return the landmark associated.
+           % 2. If the point is very close to an existing landmark, 
+           %    but not close enough to be associated. Discard both 
+           %    the point and the landmark as it is not distinguishable.
+           % 3. Otherwise, consider it as a new landmark which will be
+           %    added to the landmark database after EKF update.
+
+           lm = false;
+           for i = 1:length(obj.landmarks)
+               cLm = obj.landmarks(i);
+               if norm([x y] - cLm.position) < obj.maxAssociateError
+                   lm = cLm;
+                   break;
+               end
+           end
+           
+           % If the point is not close enough to an existing landmark,
+           % store it in obj.newLandmarks.
+           if ~isa(lm, 'Landmark')
+               tooClose = false;  
+               for i = 1:length(obj.landmarks)
+                   cLm = obj.landmarks(i);
+                   if norm([x y] - cLm.position) < obj.maxLandmarkError
+                       tooClose = true;
+                       break;
+                   end
+               end
+
+               if tooClose 
+                   for j = i+1:length(obj.landmarks)
+                       obj.landmarks(j).id = obj.landmarks(j).id - 1;
+                   end 
+                   obj.landmarks(i) = [];
+               end
+               
+               if ~tooClose
+                   lm = Landmark();
+                   lm.position = [x y];
+
+                   if isempty(obj.newLandmarks)
+                       lm.id = length(obj.landmarks) + 1;
+                   else
+                       lm.id = obj.newLandmarks(length(obj.newLandmarks)).id + 1;
+                   end
+
+                   obj.newLandmarks = [obj.newLandmarks lm];
+                   
+               end
+           end
+        end
+
+        function addNewLandmarks(obj)
+           % Add new landmarks to our database and set it to be empty.
+           obj.landmarks = [obj.landmarks obj.newLandmarks];
+           obj.newLandmarks = [];
+        end
+    end
+    
+    methods(Static)
+        function s = test()
+           DB = LandmarkDatabase();
+           DB.extractLandmarks([1 2; 3 4]);
+           DB.addNewLandmarks();
+
+           disp(['You should see two landmarks in the database.']);
+           DB
+           DB.landmarks(1)
+           DB.landmarks(2)
+
+           disp(['New observations are [1.001 2.001] and [3.1 4.1].']);
+           disp(['[1.001 2.001] would be associated with [1 2].']);
+           disp(['[3.1 4.1] is very close to [3 4] and both ', ... 
+            'of them will not be good landmarks.'])
+           disp(['You should see one landmark in the database.']);
+           DB.extractLandmarks([1.001 2.001; 3.1 4.1]);
+           DB
+           DB.landmarks(1)
+
+           disp(['You should see no changes.']);
+           DB.addNewLandmarks();
+           DB
+           DB.landmarks(1)
+        end
+    end
+end
